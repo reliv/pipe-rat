b7a594c95f4d4bb3a311d2b161b52af37ef58820
James Jervis <jjervis@relivinc.com>
2016-04-29 12:27:53 -0500

    Implement Hydrators and defualt config extension

---
+++ src/Middleware/ResponseFormat/FileResponseFormat.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Reliv\PipeRat\Middleware\ResponseFormat;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Reliv\PipeRat\Exception\ResponseFormatException;
+use Reliv\PipeRat\Middleware\Middleware;
+
+/**
+ * Class FileResponseFormat
+ *
+ * @author    James Jervis <jjervis@relivinc.com>
+ * @copyright 2016 Reliv International
+ * @license   License.txt
+ * @link      https://github.com/reliv
+ */
+class FileResponseFormat extends AbstractResponseFormat implements Middleware
+{
+    /**
+     * @var array
+     */
+    protected $defaultAcceptTypes= [
+            'application/json'
+        ];
+
+    /**
+     * __invoke
+     *
+     * @param Request       $request
+     * @param Response      $response
+     * @param callable|null $next
+     *
+     * @return \Psr\Http\Message\MessageInterface
+     * @throws ResponseFormatException
+     */
+    public function __invoke(Request $request, Response $response, callable $next = null)
+    {
+        if (!$this->isValidAcceptType($request)) {
+            return $next($request, $response);
+        }
+        $dataModel = $this->getDataModel($response);
+
+        // @todo
+
+//        return $response->withBody($body)->withHeader(
+//            'Content-Type',
+//            'application/json'
+//        );
+    }
+}
