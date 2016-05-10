634b790319a162d57694861286d7b285dbc05107
rodmcnew <rodmcnew@gmail.com>
2016-05-10 10:56:08 -0500

    fix rcmuser acl

--- src/Middleware/Acl/RcmUserAcl.php
+++ src/Middleware/Acl/RcmUserAcl.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Reliv\PipeRat\Acl\Middleware;
+namespace Reliv\PipeRat\Middleware\Acl;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
