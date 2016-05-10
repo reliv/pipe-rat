0794b651fea2fb37b63eb22eb9efa2d7615c6b7f
James Jervis <jjervis@relivinc.com>
2016-05-10 09:36:42 -0500

    Tests

---
+++ test-mock/Psr/Http/Message/ServerRequestInterfaceMock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Reliv\PipeRat\TestMock\Psr\Http\Message;
+
+/**
+ * Class ServerRequestInterfaceMock
+ *
+ * @author    James Jervis <jjervis@relivinc.com>
+ * @copyright 2016 Reliv International
+ * @license   License.txt
+ * @link      https://github.com/reliv
+ */
+class ServerRequestInterfaceMock extends Mock
+{
+
+}
---
+++ test/Middleware/Extractor/CollectionPropertyGetterExtractorTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Reliv\PipeRat\Test\Middleware\Extractor;
+
+class CollectionPropertyGetterExtractorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
